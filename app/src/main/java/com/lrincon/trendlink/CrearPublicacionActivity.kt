3b292e8941081df8177fa4bcb12f7351fe76d561
CatConstellation <160039566+CatConstellation@users.noreply.github.com>
2024-05-14 16:29:12 -0500

    Add files via upload

--- app/src/main/java/com/lrincon/trendlink/CrearPublicacionActivity.kt
+++ app/src/main/java/com/lrincon/trendlink/CrearPublicacionActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.trendlinkvalentina.R
 
-class CreateAccountActivity : AppCompatActivity() {
+class CrearPublicacionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_create_account)
+        setContentView(R.layout.activity_crear_publicacion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
