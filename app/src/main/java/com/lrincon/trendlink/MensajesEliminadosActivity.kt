9bf83fe26a7cf7dfd9f0c15c9db4fe99d62efdaf
Laura Rincón <81595177+LauraR287@users.noreply.github.com>
2024-05-14 08:32:04 -0500

    Changes

---
+++ app/src/main/java/com/lrincon/trendlink/MensajesEliminadosActivity.kt
@@ -0,0 +1,11 @@
+package com.lrincon.trendlink
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MensajesEliminadosActivity: AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.mensajes_eliminados)
+        }
+}
