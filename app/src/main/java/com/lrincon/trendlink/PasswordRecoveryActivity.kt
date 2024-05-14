d5ca34d1ff6e93b647cf83f8d864091207cd914f
Laura Rincón <81595177+LauraR287@users.noreply.github.com>
2024-05-13 21:30:38 -0500

    Changes recover password

--- app/src/main/java/com/lrincon/trendlink/PasswordRecoveryActivity.kt
+++ app/src/main/java/com/lrincon/trendlink/PasswordRecoveryActivity.kt
@@ -1,33 +1,37 @@
 package com.lrincon.trendlink
 
-/*
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
-class PasswordRecoveryActivity: AppCompatActivity() {
-    private lateinit var email: EditText
-    private lateinit var code: Button
+class PasswordRecoveryActivity : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var saveButton: Button
+    private lateinit var currentUser: FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
-        email = findViewById(R.id.email)
-        code = findViewById(R.id.code)
-        val auth = FirebaseAuth.getInstance()
+        emailEditText = findViewById(R.id.email)
+        saveButton = findViewById(R.id.code)
+        currentUser = FirebaseAuth.getInstance().currentUser!!
 
-        recuperarContraseña();
+        saveButton.setOnClickListener {
+            val email = emailEditText.text.toString().trim()
 
-    }
-
-    private fun recuperarContraseña() {
-
-        code.setOnClickListener(new View . OnClickListener)
+            if (email.isNotEmpty()) {
+                Toast.makeText(this, "Sigue las instrucciones enviadas a tu correo", Toast.LENGTH_SHORT).show()
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                finish()
+            } else {
+                Toast.makeText(this, "Por favor ingresa un correo", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
-*/
+
